8f8e780965d32ad7be3f594f4ce602fb22650224
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-24 10:06:40 -0300

    Consumindo informação ddo aruivo  json

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,13 @@ function updateProfileInfo(profileData){
 
     const localization = document.getElementById('profile.localization')
     localization.innerText = profileData.localization
+    
+    const linkedin = document.getElementById('profile.linkedin')
+    linkedin.innerText = profileData.linkedin
 
+    const github = document.getElementById('profile.github')
+    github.innerText = profileData.github
+    
     const phone  = document.getElementById('profile.phone')
     phone.innerText = profileData.phone
 
