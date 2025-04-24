0201e84b970424a7ed239df070e62d02feb609a7
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-24 09:56:26 -0300

    Consumindo APi do gitHub

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,5 @@
 
+
 async function fetchProfileData(){
     const url = 'https://raw.githubusercontent.com/Iranlsjr/js-developer-portfolio/refs/heads/main/assets/data/profile.json'
     const fetching = await fetch(url)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
-(async function (){
-    const profileData = await fetchProfileData()
-    console.log(profileData)
+function updateProfileInfo(profileData){
+    const  photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
 
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const localization = document.getElementById('profile.localization')
+    localization.innerText = profileData.localization
+
+
+
+
+}
+
+(async  () => {
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
 })()
