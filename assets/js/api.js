6350702a4d56f2de7ac0a8c32ed8c77ffbc227cc
Iranlsjr <iran.lourenco.jr@gmail.com>
2025-04-26 16:49:39 -0300

    Atualizando o URL para o link atual

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/Iranlsjr/js-developer-portfolio/refs/heads/main/assets/data/profile.json'
+    const url = 'https://raw.githubusercontent.com/Iranlsjr/portfolio/refs/heads/main/assets/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
